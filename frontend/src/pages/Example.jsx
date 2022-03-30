6f842a9e21a423add684d0f6673bf72956638eb0
yang020501 <thaiduong020501@gmail.com>
2022-03-30 09:55:07 +0700

    Update

---
+++ frontend/src/pages/Example.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Example = () => {
+  return (
+    <div><h1>Example</h1></div>
+  )
+}
---
+++ frontend/src/pages/routing/ProtectedRoute.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Route, Navigate ,Outlet, Routes } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+import { Spinner } from 'react-bootstrap'
+
+
+const ProtectedRoute = ({component: Component, ...rest}) => {
+
+    const { authState: { authLoading, isAuthenticated } } = useContext(AuthContext)
+
+    if (authLoading) {
+        return (
+            <div className="spinner-container">
+                <Spinner animation='border' variant='info' />
+            </div>
+           
+        )
+    }
+
+    return (
+        <Route {...rest} render={props => isAuthenticated ? 
+            (
+                
+                    
+                    <Component {...props} {...rest} />
+            )
+                
+             :
+            <Navigate to='/login'/>
+        }/>
+       
+    )
+}
+
+export default ProtectedRoute
