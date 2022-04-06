f224898d50c979894812cdf97ce99c701c69e676
yang020501 <thaiduong020501@gmail.com>
2022-04-06 20:11:05 +0700

    fix

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { logo } from '../../assets/img'
+
 import './header.css'
 
 const Header = () => {
@@ -7,12 +9,16 @@ const Header = () => {
             <nav class="navbar navbar-expand-lg fixed-top sticky-top navbar-light bg-white">
                 <div class="container-fluid">
                     <div class="d-flex">
-                        <a href="" class="navbar-brand text-uppercase font-weight-bold"><span>SimpleCode</span></a>
+
+                        <a href="/main" class="navbar-brand text-uppercase font-weight-bold">
+                            
+                            <span>SimpleCode</span>
+                            </a>
                     </div>
                     <span class="d-flex" id="homeTxt">Home</span>
                     <div class="d-flex col-1 ">
-                        <a class="btn  btn-lg btn-floating  avatar-a"  href =""type='button' role={'button'}></a>
-                        
+                        <a class="btn  btn-lg btn-floating  avatar-a" href="" type='button' role={'button'}></a>
+
                     </div>
 
                 </div>
