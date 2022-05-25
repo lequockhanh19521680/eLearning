e88d43d685a99026b4d4da67d8b3e91674bed09e
yang020501 <thaiduong020501@gmail.com>
2022-05-25 18:58:28 +0700

    aha

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ImageGallery from '../../components/ImageIntro'
+import Banner from '../../components/Banner'
+const Home = () => {
+    return (
+        <React.Fragment>
+            <Banner />
+            <ImageGallery></ImageGallery>
+        </React.Fragment>
+
+    )
+}
+
+export default Home
