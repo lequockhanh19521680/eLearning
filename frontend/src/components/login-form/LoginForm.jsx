8d99c05b770287acaeda180abed9b66ffcca78c7
yang020501 <thaiduong020501@gmail.com>
2022-05-29 14:58:57 +0700

    update sth

--- frontend/src/components/login-form/LoginForm.jsx
+++ frontend/src/components/login-form/LoginForm.jsx
@@ -8,63 +8,68 @@ import { Loading } from '../loading/Loading'
 
 
 const LoginForm = () => {
-
     let navigate = useNavigate();
-   
+    const [loading, setLoading] = useState(false)
+    const [t] = useState(<Loading />)
 
     // Data trong form
     const [loginForm, setLoginForm] = useState({
         username: '',
         password: '',
     })
-    
+
     // cập nhật dữ liệu khi mà nhập trong input
     const onChangeLoginForm = e => setLoginForm({
         ...loginForm,
         [e.target.name]: e.target.value
-    },setAlert(null))
+    }, setAlert(null))
 
 
     // Xử lý đoạn submit form
-    const { loginUser} = useContext(AuthContext)
+    const { loginUser } = useContext(AuthContext)
     const [alert, setAlert] = useState(null)
-    
+
 
     // khi ấn submit form   
-    const handleLogin = async (e) => {
+    const handleLogin = (e) => {
         e.preventDefault()
-        try {
-           
-            const result = await loginUser(loginForm)
-            if(result === undefined)
-            {
-                console.log(result)
-                console.log("waiting....")
-                return <Loading/>
+        setLoading(true)
+        setTimeout(async () => {
+            try {
+                const result = await loginUser(loginForm)
+                if (result === undefined) {
+                    console.log(result)
+                    console.log("waiting....")
+
+                }
+                if (result.success) {
+                    setAlert(null)
+
+                    navigate('/main')
+                } else {
+
+                    setAlert({ type: 'danger', message: result.message })
+
+                    //setTimeout(() => setAlert(null), 5000)
+                }
+            } catch (error) {
+                console.log(error)
             }
-           
-            if (result.success) {                
-                setAlert(null)
-                navigate('/main')
-            } else {
-                
-                setAlert({ type: 'danger', message: result.message })
-                //setTimeout(() => setAlert(null), 5000)
-            }
-        } catch (error) {
-            console.log(error)
-        }
+            setLoading(false)
+        }, 1000)
+
     }
+
 
     return (
         <>
-            <section className="login-clean">
+            <section className={`login-clean ${loading ? "opacity" : ""}`}>
                 <div className="container flex-column">
                     <div className="row align-items-center padding-mobile">
                         <div className="col-md-6 col-align-left"><img id="logo" src={logo} alt='logo' /></div>
                         <div className="col-md-6 col-align-right padding-top-18" style={{ marginTop: '50px' }}>
                             <form onSubmit={handleLogin}>
-                                
+
                                 <AlertMessage info={alert} />
                                 <h2 className="visually-hidden">Login Form</h2>
                                 <div className="illustration">
@@ -86,7 +91,7 @@ const LoginForm = () => {
                     </div>
                 </div>
             </section>
-        
+            {loading && <Loading />}
 
 
         </>
